7772277b115268ce8edecb9728a5039df0f00ec9
ceptic0n <naquino546@gmail.com>
2024-02-09 19:34:34 -0800

    added logic for update monthly, and update. Need to fix visuals in html now

--- jasmineExercise/calculator/calculator.js
+++ jasmineExercise/calculator/calculator.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
-    setupIntialValues();
+    setupInitialValues();
     form.addEventListener("submit", function(e) {
       e.preventDefault();
       update();
@@ -20,30 +20,34 @@ function getCurrentUIValues() {
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
-function setupIntialValues() {
-  let loanAmount = document.getElementById("loan-amount");
-  let loanYears = document.getElementById("load-years");
-  let loanRate = document.getElementById("loan-rate");
+function setupInitialValues() {
+  const loanAmount = document.getElementById("loan-amount");
+  const loanYears = document.getElementById("loan-years");
+  const loanRate = document.getElementById("loan-rate");
+
+  const monthlyPayment = document.getElementById("monthly-payment");
 
   const values = {
-    amount : loanAmount,
-    years : loanYears,
-    rate : loanRate
+    amount : loanAmount.value,
+    years : loanYears.value,
+    rate : loanRate.value
   };
 
-  calculateMonthlyPayment(values);
+  updateMonthly(calculateMonthlyPayment(values));
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(values));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  let principle = values.amonut;
+  let principle = values.amount;
   let periodicIR = values.rate/12;
   let paymentTotal = values.years * 12;
 
@@ -54,4 +58,10 @@ function calculateMonthlyPayment(values) {
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyPayment = document.getElementById("monthly-payment");
+  if(monthlyPayment.innerText == "NaN"){
+    monthlyPayment.innerText = " ";
+  } else{
+  monthlyPayment.innerText = monthly;
+  }
 }
