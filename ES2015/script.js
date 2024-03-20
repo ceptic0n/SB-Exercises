c2287d6e278702920914c30e83a062e09ff7e105
ceptic0n <naquino546@gmail.com>
2024-03-20 14:17:31 -0700

    created files for rest/spread exercises

--- ES2015/script.js
+++ ES2015/script.js
@@ -1,1 +1,24 @@
 console.log(eek);
+
+
+function eek(){
+    console.log(arguments);
+}
+
+eek(0,1,2,3,4,5,6,7,8,9);
+
+
+
+function printNums(...nums){
+    console.log(nums);
+}
+
+printNums(0,1,2,4,5,6,7);
+
+function songsByArtist(artist, ...songs){
+    songs.forEach(el => {
+        console.log(`${el} by ${artist}`);
+    });
+}
+
+songsByArtist("Porter Robinson", "Cheerleader", "Look at the Sky", "Divinity");
