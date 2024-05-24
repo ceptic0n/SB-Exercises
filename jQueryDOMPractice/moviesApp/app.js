f1a2740927009544e9545cd5ff707438a3f87c77
ceptic0n <naquino546@gmail.com>
2024-05-24 13:55:20 -0700

    Finished movieApp

--- jQueryDOMPractice/moviesApp/app.js
+++ jQueryDOMPractice/moviesApp/app.js
@@ -1,1 +1,48 @@
 console.log("eek")
+
+$("body").on("click", function(e){
+    if($(e.target).is("input[type='submit']")){
+        e.preventDefault();
+
+        if(testIfMovieisValid() && testValidRating()){
+        $(".movieList").append(formattedMovieRating());
+        }
+    }
+
+    if(e.target.tagName === "BUTTON"){
+        $(e.target).parent().remove();
+    }
+});
+
+
+//function to format rating + movie title
+
+function formattedMovieRating(){
+    let movieTitle = $(".movieTitle").val();
+    let movieRating = $(".movieRating").val();
+    return `<li>"${movieTitle}" - ${movieRating}/10 <button>Remove</button> </li>`
+}
+
+function testIfMovieisValid(){
+    if($(".movieTitle").val().length < 2){
+        alert("Movie Title must be more than 2 characters.")
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function testValidRating(){
+    if($(".movieRating").val() === ""){
+        alert("Please enter a rating");
+        return false;
+    }
+
+    if($(".movieRating").val() < 0 || $(".movieRating").val() > 10){
+        alert("Movie Rating must be 0 - 10")
+        return false;
+    } else {
+        return true;
+    }
+}
+
