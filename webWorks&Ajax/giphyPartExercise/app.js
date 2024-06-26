94912dd8d39943fe8299931c265773e6acee0c52
ceptic0n <58377659+ceptic0n@users.noreply.github.com>
2024-06-26 03:00:58 -0700

    progress

--- webWorks&Ajax/giphyPartExercise/app.js
+++ webWorks&Ajax/giphyPartExercise/app.js
@@ -1,1 +1,19 @@
 console.log("Let's get this party started!");
+
+const KEY = "Q0yE0f71P3haCUOm8nUYXjj3qTy5oVPN"
+
+
+async function getRandomResponse(){
+    let response = await axios.get("https://api.giphy.com/v1/gifs/random", {params: {api_key: KEY}});
+    console.log(response);
+}
+
+async function getRandomGifURL(){
+    let response = await axios.get("https://api.giphy.com/v1/gifs/random", {params: {api_key: KEY}});
+    console.log(response.data.data.images.original.url);
+}
+
+function appendGif(gifURL){
+    let newImg = $(`'<img src="${gifURL}"/>'`)
+    $('#gifCollection').append(newImg);
+};
