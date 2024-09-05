063845363e51951a44aa48ed30f3240004465c72
ceptic0n <naquino546@gmail.com>
2024-09-04 17:53:11 -0700

    mState

--- reactIntro/viteEX/src/App.jsx
+++ reactIntro/viteEX/src/App.jsx
@@ -6,6 +6,7 @@ import HEADER from './components/HEADER'
 import Clicker from './components/Clicker'
 import ButtonGroup from './components/ButtonGroup'
 import NumberGame from './components/NumberGame'
+import MyState from './components/MyState'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +14,7 @@ function App() {
   return (
     <>
       <HEADER />
-      <NumberGame />
+      <MyState/>
     </>
   )
 }
---
+++ reactIntro/viteEX/src/components/MyState.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+    const players = ["mario", "luigi", "peach"];
+
+const myState = () => {
+  return (
+    <div>{players[0]}</div>
+  )
+}
+
+export default myState
